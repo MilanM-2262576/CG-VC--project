a081ae1d5d6cf47bd43abe1839fca3653e0dfb64
MilanM-2262576 <114240241+MilanM-2262576@users.noreply.github.com>
2025-05-14 22:01:14 +0200

    merge

--- Project CG&VC/main.cpp
+++ Project CG&VC/main.cpp
@@ -55,106 +55,36 @@ int main() {
 
 
 	std::vector<std::vector<glm::vec3>> bezierSegments = {
-		// Lange rechte start
+		// Start links-voor
 		{
-			{0.0f, 1.0f, 0.0f},   // Startpunt
-			{10.0f, 1.0f, 0.0f},  // Rechte lijn
-			{20.0f, 1.0f, 0.0f},  // Rechte lijn
-			{30.0f, 1.0f, 0.0f}   // Rechte lijn
+			{-120.0f, 30.0f, -120.0f},
+			{-80.0f, 35.0f, -80.0f},
+			{-40.0f, 50.0f, -40.0f},   // Begin klim
+			{0.0f, 70.0f, 0.0f}        // Top van de berg
 		},
-		// Zachte klim
+		// Midden naar rechts-achter (afdaling)
 		{
-			{30.0f, 1.0f, 0.0f},
-			{35.0f, 3.0f, 2.0f},  // Begin klim
-			{40.0f, 5.0f, 4.0f},  // Top van de klim
-			{45.0f, 3.0f, 6.0f}   // Overgang naar vlak stuk
+			{0.0f, 70.0f, 0.0f},       // Top van de berg
+			{40.0f, 50.0f, 40.0f},     // Daling
+			{80.0f, 35.0f, 80.0f},
+			{120.0f, 30.0f, 120.0f}
 		},
-		// Zachte daling
+		// Bocht naar links-achter
 		{
-			{45.0f, 3.0f, 6.0f},
-			{50.0f, 2.0f, 8.0f},  // Halverwege daling
-			{55.0f, 1.5f, 10.0f}, // Bijna bodem
-			{60.0f, 1.0f, 12.0f}  // Bodem
+			{120.0f, 30.0f, 120.0f},
+			{100.0f, 40.0f, 60.0f},
+			{60.0f, 35.0f, 100.0f},
+			{120.0f, 30.0f, -120.0f}
 		},
-		// Bocht naar rechts
+		// Bocht naar rechts-voor en terug naar start
 		{
-			{60.0f, 1.0f, 12.0f},
-			{65.0f, 1.0f, 16.0f},  // Begin bocht
-			{70.0f, 1.0f, 20.0f},  // Midden bocht
-			{75.0f, 1.0f, 24.0f}   // Einde bocht
-		},
-		// Kleine klim en daling
-		{
-			{75.0f, 1.0f, 24.0f},
-			{80.0f, 2.0f, 26.0f},  // Kleine klim
-			{85.0f, 2.0f, 28.0f},  // Vlak stuk
-			{90.0f, 1.0f, 30.0f}   // Kleine daling
-		},
-		// Bocht naar links
-		{
-			{90.0f, 1.0f, 30.0f},
-			{95.0f, 1.0f, 34.0f},  // Begin bocht
-			{100.0f, 1.0f, 38.0f}, // Midden bocht
-			{105.0f, 1.0f, 42.0f}  // Einde bocht
-		},
-		// Zachte klim
-		{
-			{105.0f, 1.0f, 42.0f},
-			{110.0f, 3.0f, 44.0f}, // Begin klim
-			{115.0f, 5.0f, 46.0f}, // Top van de klim
-			{120.0f, 3.0f, 48.0f}  // Overgang naar daling
-		},
-		// Zachte daling
-		{
-			{120.0f, 3.0f, 48.0f},
-			{125.0f, 2.0f, 50.0f}, // Halverwege daling
-			{130.0f, 1.5f, 52.0f}, // Bijna bodem
-			{135.0f, 1.0f, 54.0f}  // Bodem
-		},
-		// Eindbocht
-		{
-			{135.0f, 1.0f, 54.0f},
-			{140.0f, 1.0f, 58.0f},  // Begin bocht
-			{145.0f, 1.0f, 62.0f},  // Midden bocht
-			{150.0f, 1.0f, 66.0f}   // Einde bocht
-		},
-		// Extra segmenten om de achtbaan te verlengen
-		// Rechte lijn
-		{
-			{150.0f, 1.0f, 66.0f},
-			{160.0f, 1.0f, 70.0f},
-			{170.0f, 1.0f, 74.0f},
-			{180.0f, 1.0f, 78.0f}
-		},
-		// Zachte klim
-		{
-			{180.0f, 1.0f, 78.0f},
-			{185.0f, 3.0f, 80.0f},
-			{190.0f, 5.0f, 82.0f},
-			{195.0f, 3.0f, 84.0f}
-		},
-		// Zachte daling
-		{
-			{195.0f, 3.0f, 84.0f},
-			{200.0f, 2.0f, 86.0f},
-			{205.0f, 1.5f, 88.0f},
-			{210.0f, 1.0f, 90.0f}
-		},
-		// Bocht naar rechts
-		{
-			{210.0f, 1.0f, 90.0f},
-			{215.0f, 1.0f, 94.0f},
-			{220.0f, 1.0f, 98.0f},
-			{225.0f, 1.0f, 102.0f}
-		},
-		// Eindbocht
-		{
-			{225.0f, 1.0f, 102.0f},
-			{230.0f, 1.0f, 106.0f},
-			{235.0f, 1.0f, 110.0f},
-			{240.0f, 1.0f, 114.0f}
+			{120.0f, 30.0f, -120.0f},
+			{60.0f, 40.0f, -100.0f},
+			{-60.0f, 35.0f, -100.0f},
+			{-120.0f, 30.0f, -120.0f}
 		}
 	};
+
 
 	RollerCoaster rollerCoaster(bezierSegments, 0.5f, 16); // Create a rollercoaster
 
@@ -252,14 +182,10 @@ int main() {
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 10000.0f);
 		glm::mat4 view = camera.GetViewMatrix();
 
-		// Render the heightmap
-		glm::mat4 heightmapModel = glm::mat4(1.0f);
-		heightmap.Render(projection, view, heightmapModel);
-
 		// Render the rollercoaster
 		rollerCoaster.Render(projection, view);
 
-    // Render the cart
+		// Render the cart
 		cart.Update(deltaTime);
 		cart.Render(projection, view);
 
