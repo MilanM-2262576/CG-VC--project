9d9a960fbe7f929f90415efaac1c61e791c477a7
JochenK-2261803 <jochen.kasprzak@student.uhasselt.be>
2025-05-18 13:21:07 +0200

    FBO bug fixed

--- Project CG&VC/main.cpp
+++ Project CG&VC/main.cpp
@@ -67,6 +67,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 GLFWwindow* InitializeGLFW();
+void SetupFBO();
 
 int main() {
 	//Initialize GLFW window
